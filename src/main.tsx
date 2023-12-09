af34caca029210b9fa8f909c55354ffe3da98ec8
alejoboga20 <alejoboga19@gmail.com>
2023-12-09 19:30:17 -0300

    init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider } from '@nextui-org/react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.tsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <NextUIProvider>
-      <main className="dark text-foreground bg-background">
-        <RouterProvider router={ router } />
-      </main>
-    </NextUIProvider>
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<NextUIProvider>
+			<main className='dark text-foreground bg-background'>
+				<RouterProvider router={router} />
+			</main>
+		</NextUIProvider>
+	</React.StrictMode>
+);
