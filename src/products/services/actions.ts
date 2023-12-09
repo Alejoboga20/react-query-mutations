8bde4b2952c81d23afb9f4c5fe2e4b6423d58d59
alejoboga20 <alejoboga19@gmail.com>
2023-12-09 20:44:47 -0300

    sleep

--- src/products/services/actions.ts
+++ src/products/services/actions.ts
@@ -4,7 +4,17 @@ interface GetProductsArgs {
 	filterKey?: string;
 }
 
+const sleep = (seconds: number = 0): Promise<boolean> => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(true);
+		}, seconds * 1000);
+	});
+};
+
 export const getProducts = async ({ filterKey }: GetProductsArgs) => {
+	sleep(2);
+
 	const { data } = await productsApi.get<Product[]>(`/products`, {
 		params: {
 			category: filterKey,
