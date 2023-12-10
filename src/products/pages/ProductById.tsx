fb7a64009163b713d5eb9d0e6aa1c853d60d4922
alejoboga20 <alejoboga19@gmail.com>
2023-12-09 21:17:22 -0300

    scroll to top

--- src/products/pages/ProductById.tsx
+++ src/products/pages/ProductById.tsx
@@ -1,9 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { ProductCard, useProduct } from '..';
+import { useEffect } from 'react';
 
 export const ProductById = () => {
 	const { id } = useParams<{ id: string }>();
 	const { data } = useProduct({ id: +id! });
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
 	return (
 		<div className='flex-col'>
