5300cf81003a166ac0e6e3af890455723de575be
alejoboga20 <alejoboga19@gmail.com>
2023-12-13 15:47:33 -0300

    invalidate query

--- src/products/hooks/useProductMutation.ts
+++ src/products/hooks/useProductMutation.ts
@@ -1,11 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { productActions } from '..';
 
 export const useProductMutation = () => {
+	const queryClient = useQueryClient();
+
 	const mutation = useMutation({
 		mutationFn: productActions.createPRoduct,
-		onSuccess: () => {
-			console.log('first mutation success');
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({
+				queryKey: ['products', { filterKey: data.category }],
+			});
 		},
 		onSettled: () => {
 			console.log('first mutation settled');
